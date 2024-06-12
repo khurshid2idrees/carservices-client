fbbb1ea53846e1ebb82613d2f540617bd38c2731
khurshid idrees <umarkhurshid3@gmail.com>
2024-06-12 08:51:29 +0530

    update in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -58,12 +58,13 @@ export default function NavBar() {
                       Home
                     </Link>
                   </li>
+
                   <li>
                     <Link
                       className="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2"
                       to={`/cart`}
                     >
-                      Cart ({products.length > 0 ? products.length : null})
+                      Cart
                     </Link>
                   </li>
                   <li>
@@ -86,6 +87,30 @@ export default function NavBar() {
               </button> */}
 
               <div className="auth flex items-center w-full md:w-full">
+                <div className="mr-8">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-8 w-8"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                    />
+                  </svg>
+                  <span class="flex absolute -mt-8 ml-4">
+                    <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-black opacity-75"></span>
+                    <span class="relative inline-flex rounded-full h-6 w-6 bg-black">
+                      <span className="ml-2 text-white">
+                        {products.length > 0 ? products.length : null}
+                      </span>
+                    </span>
+                  </span>
+                </div>
                 {user.name ? (
                   <>
                     <button
