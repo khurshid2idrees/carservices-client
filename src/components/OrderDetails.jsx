589fbdff4b077906090c128e636629d40da270c9
khurshid idrees <umarkhurshid3@gmail.com>
2024-06-12 14:00:18 +0530

    update in orderdetails

--- src/components/OrderDetails.jsx
+++ src/components/OrderDetails.jsx
@@ -12,6 +12,10 @@ export default function OrderDetails() {
   const cartitems = useSelector((state) => state.cart.cartItems);
 
   const { cart } = orders[0];
+  const {address} = orders[0]
+  
+
+  console.log(address,'address mil gya bhai')
 
   const allprice = cart.map((data) => Number(data.price) * data.quantity);
   const totalPrice = allprice.reduce((a, b) => a + b, 0);
@@ -151,11 +155,11 @@ export default function OrderDetails() {
                     <div className="flex w-16 justify-center  md:justify-start items-center space-x-4 py-8 border-b border-gray-200">
                       <div className="flex justify-start items-start flex-col space-y-2">
                         <p className="text-base dark:text-white font-semibold leading-4 text-left text-gray-800">
-                          David Kent
+                          {user.name}
                         </p>
-                        <p className="text-sm dark:text-gray-300 leading-5 text-gray-600">
+                        {/* <p className="text-sm dark:text-gray-300 leading-5 text-gray-600">
                           10 Previous Orders
-                        </p>
+                        </p> */}
                       </div>
                     </div>
                     <div className="flex justify-center text-gray-800 dark:text-white md:justify-start items-center space-x-4 py-4 border-b border-gray-200 w-full">
@@ -180,28 +184,21 @@ export default function OrderDetails() {
                         />
                       </svg>
                       <p className="cursor-pointer text-sm leading-5 ">
-                        david89@gmail.com
+                        {user.email}
                       </p>
                     </div>
                   </div>
                   <div className="flex justify-between xl:h-full items-stretch w-full flex-col mt-6 md:mt-0">
-                    <div className="flex justify-center md:justify-start xl:flex-col flex-col md:space-x-6 lg:space-x-8 xl:space-x-0 space-y-4 xl:space-y-12 md:space-y-0 md:flex-row items-center md:items-start">
+                    <div className="flex justify-center md:mt-8 md:justify-start xl:flex-col flex-col md:space-x-6 lg:space-x-8 xl:space-x-0 space-y-4 xl:space-y-12 md:space-y-0 md:flex-row items-center md:items-start">
                       <div className="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4 xl:mt-8">
                         <p className="text-base dark:text-white font-semibold leading-4 text-center md:text-left text-gray-800">
                           Shipping Address
                         </p>
                         <p className="w-48 lg:w-full dark:text-gray-300 xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">
-                          180 North King Street, Northhampton MA 1060
+                         {address.address},{address.city},{address.state}, {address.zip}
                         </p>
                       </div>
-                      <div className="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4">
-                        <p className="text-base dark:text-white font-semibold leading-4 text-center md:text-left text-gray-800">
-                          Billing Address
-                        </p>
-                        <p className="w-48 lg:w-full dark:text-gray-300 xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">
-                          180 North King Street, Northhampton MA 1060
-                        </p>
-                      </div>
+                    
                     </div>
                   </div>
                 </div>
